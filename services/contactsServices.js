c7e444e8fdaa27ef46e776ecb4f8d2172d7e2d96
nataliyatraxler <nataliya.traxler@gmail.com>
2025-07-03 00:55:57 +0200

    fix

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,21 +1,19 @@
 const Contact = require("../models/contact");
 
-// Получить все контакты
+
 const listContacts = async () => {
   return await Contact.findAll();
 };
 
-// Получить контакт по ID
 const getContactById = async (id) => {
   return await Contact.findByPk(id);
 };
 
-// Добавить контакт
 const addContact = async ({ name, email, phone, favorite = false }) => {
   return await Contact.create({ name, email, phone, favorite });
 };
 
-// Удалить контакт по ID
+
 const removeContact = async (id) => {
   const contact = await Contact.findByPk(id);
   if (!contact) return null;
@@ -23,7 +21,7 @@ const removeContact = async (id) => {
   return contact;
 };
 
-// Обновить контакт по ID
+
 const updateContact = async (id, data) => {
   const contact = await Contact.findByPk(id);
   if (!contact) return null;
